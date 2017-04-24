a81641efbd425276443a567b22a1468809b5d5ca
Diego G. Slomp <diegogslomp@gmail.com>
2017-04-24 16:13:48 -0300

    added --field-separator option

--- README.rst
+++ README.rst
@@ -5,9 +5,10 @@ A script to get telnet command output routers/switches layer 3. Tested on Entera
 
 ::
 
-    $ python ask-router.py -u admin -p admin_pass <target_ip> show port status
+    $ python ask-router.py <target_ip> -u admin -p 'admin_pass' show port status
     $ python ask-router.py <target_ip> show vlan portinfo > output.txt
-    $ python ask-router.py <target_ip> router, configure, show running-config, exit, exit
+    $ python ask-router.py <target_ip> router, show running-config, exit
+    $ python ask-router.py <target_ip> -F ';' 'router; enable; show interface; exit; exit'
 
 License
 -------
--- ask-router.py
+++ ask-router.py
@@ -32,14 +32,15 @@ def main(arguments):
     parser.add_argument('-u', '--user', help='Telnet user', default='user')
     parser.add_argument('-p', '--password', help='Telnet password',
                         default='secret')
+    parser.add_argument('-F', '--field-separator', help='Telnet commands separator',
+                        default=',')
     parser.add_argument('ip', help='Ip address from target')
     parser.add_argument('commands', nargs='+', help='Telnet commands, separate \
-                        with comma (,) if more than one')
+                        with comma (or custom --field-separator) if more than one')
 
     args = parser.parse_args(arguments)
-
     print(get_telnet_output(ip=args.ip, user=args.user, password=args.password,
-          commands=' '.join(args.commands).split(',')))
+          commands=' '.join(args.commands).split(args.field_separator)))
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
