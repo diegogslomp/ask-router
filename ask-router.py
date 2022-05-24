86b6f83212237c605509894da9d060a5f4b845a4
_ <diegogslomp@gmail.com>
2022-05-24 20:58:23 -0300

    format with black

--- ask-router.py
+++ ask-router.py
@@ -6,46 +6,56 @@ import argparse
 import sys
 
 
-def get_telnet_output(ip: str , user: str, password: str, commands: list) -> str:
-    output = ''
+def get_telnet_output(ip: str, user: str, password: str, commands: list) -> str:
+    output = ""
     try:
         with Telnet(ip) as tn:
             tn.read_until(b"Username:")
-            tn.write(user.encode('ascii') + b"\n")
+            tn.write(user.encode("ascii") + b"\n")
             tn.read_until(b"Password:")
-            tn.write(password.encode('ascii') + b"\n")
-            match_object = tn.expect([b"->",b"Username:"])[1]
+            tn.write(password.encode("ascii") + b"\n")
+            match_object = tn.expect([b"->", b"Username:"])[1]
             if match_object.group(0) == b"Username:":
                 raise PermissionError("INVALID USER/PASSWORD")
             else:
                 for command in commands:
-                    tn.write(command.encode('ascii') + b"\n")
+                    tn.write(command.encode("ascii") + b"\n")
                 tn.write(b"exit\n")
-                output = tn.read_all().decode('ascii')
+                output = tn.read_all().decode("ascii")
     except Exception as exc:
-        output = f'ERRO: {exc}'
+        output = f"ERRO: {exc}"
     finally:
         return output
 
 
 def main(arguments):
     parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
-    parser.add_argument('-u', '--user', help='Telnet user', default='user')
-    parser.add_argument('-p', '--password', help='Telnet password',
-                        default='secret')
-    parser.add_argument('-F', '--field-separator', help='Telnet commands separator',
-                        default=',')
-    parser.add_argument('ip', help='Ip address from target')
-    parser.add_argument('commands', nargs='+', help='Telnet commands, separate \
-                        with comma (or custom --field-separator) if more than one')
+    parser.add_argument("-u", "--user", help="Telnet user", default="user")
+    parser.add_argument("-p", "--password", help="Telnet password", default="secret")
+    parser.add_argument(
+        "-F", "--field-separator", help="Telnet commands separator", default=","
+    )
+    parser.add_argument("ip", help="Ip address from target")
+    parser.add_argument(
+        "commands",
+        nargs="+",
+        help="Telnet commands, separate \
+                        with comma (or custom --field-separator) if more than one",
+    )
 
     args = parser.parse_args(arguments)
-    print(get_telnet_output(ip=args.ip, user=args.user, password=args.password,
-          commands=' '.join(args.commands).split(args.field_separator)))
+    print(
+        get_telnet_output(
+            ip=args.ip,
+            user=args.user,
+            password=args.password,
+            commands=" ".join(args.commands).split(args.field_separator),
+        )
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
